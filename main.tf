492684fdddafec95de4b3ff7e25613aa873cf7b3
gituser <Gituser@EC2AMAZ-8UDHBDS>
2025-10-06 20:31:48 +0000

    Test S3 Deploy

--- main.tf
+++ main.tf
@@ -7,7 +7,6 @@ resource "aws_s3_bucket" "nadeem_bucket" {
   acl    = "private"
 
   tags = {
-    Name        = "Nadeem's S3 Bucket"
     Environment = "Development"
     Owner       = "Nadeem Ahmed Kafil Siddiqui"
     Project     = "Infra Automation"
